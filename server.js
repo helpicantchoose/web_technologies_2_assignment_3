1cbfb84999e96728c5b80086037eff2e79cf8120
PrimaryKey <skokomi396@gmail.com>
2026-01-18 22:23:06 +0500

    Refactor server.js to use dotenv for config
    Added dotenv support for environment variables and updated MongoDB connection string to use MONGO_URI from .env file.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; 
 require('dns').setServers(['1.1.1.1', '8.8.8.8']);
 
 
 app.use(express.json());
 app.use(express.static('public')); 
 
-mongoose.connect('mongodb+srv://AbrayevDaniyal:9078563412@dogplush.zb2xhmx.mongodb.net/')
+
+if (!process.env.MONGO_URI) {
+    console.error("FATAL ERROR: MONGO_URI is not defined in .env file");
+    process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
